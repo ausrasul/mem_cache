ea77e711d1f16d04d1255c7efe46087cb0b4b271
fredrika <fredrik.ahs@mobilearts.com>
2022-01-20 08:59:16 +0100

    Add RWMutex for concurrency

--- mem_cache.go
+++ mem_cache.go
@@ -1,18 +1,26 @@
 package main
 
+import "sync"
+
 type Cache struct {
 	storage map[string]interface{}
+	mtx     *sync.RWMutex
 }
 
 func NewCache() Cache {
 	return Cache{
 		storage: make(map[string]interface{}),
+		mtx:     &sync.RWMutex{},
 	}
 }
 
 func (c Cache) Read(key string) interface{} {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
 	return c.storage[key]
 }
 func (c Cache) Write(key string, val interface{}) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
 	c.storage[key] = val
 }
