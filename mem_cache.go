cb46d2b5fd456907b41f36d85dbff3b797f36daa
Aus Abdalrasul <aus.rasul@mobilearts.com>
2022-01-20 09:16:42 +0100

    locks with channels

--- mem_cache.go
+++ mem_cache.go
@@ -1,26 +1,24 @@
 package main
 
-import "sync"
-
 type Cache struct {
 	storage map[string]interface{}
-	mtx     *sync.RWMutex
+	lock    chan bool
 }
 
 func NewCache() Cache {
 	return Cache{
 		storage: make(map[string]interface{}),
-		mtx:     &sync.RWMutex{},
+		lock:    make(chan bool, 1),
 	}
 }
 
 func (c Cache) Read(key string) interface{} {
-	c.mtx.RLock()
-	defer c.mtx.RUnlock()
+	c.lock <- true
+	defer func() { <-c.lock }()
 	return c.storage[key]
 }
 func (c Cache) Write(key string, val interface{}) {
-	c.mtx.Lock()
-	defer c.mtx.Unlock()
+	c.lock <- true
+	defer func() { <-c.lock }()
 	c.storage[key] = val
 }
